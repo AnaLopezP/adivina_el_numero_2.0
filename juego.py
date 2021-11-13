bf2853ddb7e20ff90b2300668ecaa62bd0e8d40a
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2021-11-13 17:48:05 +0100

    funcion IA

--- juego.py
+++ juego.py
@@ -1,17 +1,17 @@
 import random
 
-
 def menu():
     nivel_ok = False 
     while nivel_ok == False:
-        print("elija un nivel de dificultad entre 1 y 4, siendo el 1 el nivel facil")
+        print("elija un nivel de dificultad entre 1 y 4, siendo el 1 el nivel facil. Si quiere que juege la IA, escoja el nivel 5.")
         nivel = int(input())
-        if nivel < 1 and nivel > 4:
-            print("El nivel tiene que estar entre 1 y 4, no hay mas.")
+        if nivel < 1 and nivel > 5:
+            print("El nivel tiene que estar entre 1 y 5, no hay mas.")
         else:
             nivel_ok = True
 
     return nivel
+
 
 def jugar(numero, reintentos):
     print("Introduzca un numero entero.")
@@ -49,22 +49,53 @@ def calcular_reintentos(nivel):
         reintentos = 150
     return reintentos
 
+def IA(nivel):
+    numero_objetivo = 0
+    numero_popuesto = 0
+    min = 0
+    max = 1000000
+    if nivel == 5:
+        numero_objetivo = random.randint(0, 1000000)
+        numero_popuesto = random.randint(0, 1000000)
+        while numero_popuesto != numero_objetivo:
+            if numero_popuesto > numero_objetivo:
+                max = numero_popuesto
+                print("¿Es el numero " + str(numero_popuesto) +"?")
+                print("Te has pasado, el numero esta entre " + str(min) + " y " + str(max))
+            else:
+                min = numero_popuesto
+                print("¿Es el numero " + str(numero_popuesto) +"?")
+                print("Te has quedado corto, el numero esta entre  " + str(min) + " y " + str(max))
+            numero_popuesto = random.randint(min, max)
+               
+        
+        print("¿Es el numero " + str(numero_popuesto) + "?")
+        print("¡Si, enhorabuena!")
+        print("¡He ganado!")
+    return numero_popuesto
+        
+
+
 print("Inserte su nombre")
 nombre_juagdor = str(input())
 puntuacion = 0
 tabla_resultados = []
 tabla_resultados.append(nombre_juagdor)
 nivel = menu()
-reintentos = calcular_reintentos(nivel)
-puntuacion = reintentos
-print("Has elegido el nivel " + str(nivel) + ". Tienes " + str(reintentos) + " intentos y tu puntuación máxima es " + str(puntuacion) + ". Cada reintento se resta de la puntuación.")
-incognita = generar_numero(nivel)
-resultado = jugar(incognita, reintentos)
-if resultado > 1:
-    print("Has ganado.")
-    
+if nivel == 5:
+    ia = IA(nivel)
 else:
-    print("Game Over.")
-puntuacion = resultado - 1
-tabla_resultados.append(puntuacion)
-print(tabla_resultados)
+    reintentos = calcular_reintentos(nivel)
+    puntuacion = reintentos
+    print("Has elegido el nivel " + str(nivel) + ". Tienes " + str(reintentos) + " intentos y tu puntuación máxima es " + str(puntuacion) + ". Cada reintento se resta de la puntuación.")
+    incognita = generar_numero(nivel)
+    resultado = jugar(incognita, reintentos)
+    if resultado > 1:
+        print("Has ganado.")
+        
+    else:
+        print("Game Over.")
+    puntuacion = resultado - 1
+    tabla_resultados.append(puntuacion)
+    print(tabla_resultados)
+
